79d6d3498f6c047ddccfff44e8e906fb9ce8e68f
katklat <kinz@pm.me>
2020-05-05 18:15:58 +0200

    Remove search function from some views

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,10 +1,10 @@
 <header>
     <div class="header fixed-top">
         <div class="container d-flex">
-            <a href="{{route('lists.index')}}"> <img src="/images/back.svg" class="filter-primary inline" width="44" height="44" title="add" alt=""> </a>
-            @if (!Request::is('*create*'))
+            <a href="{{route('lists.index')}}"> <img src="/images/back.svg" class="filter-primary inline" width="48" height="48" title="add" alt=""> </a>
+            @unless (Request::is('*create*')||Request::is('*tools*'))
 
-            <a href="{{route('lists.create')}}"> <img src="/images/plus.svg" class="filter-primary inline mt-1 mr-2" width="36" height="36" title="add" alt=""></a>
+            <a href="{{route('lists.create')}}"> <img src="/images/plus.svg" class="filter-primary inline mt-2 mr-2" width="32" height="32" title="add" alt=""></a>
 
             <form method="GET" class="form-inline my-2 my-lg-0" action="">
                 @csrf
@@ -12,14 +12,15 @@
                     <input name='q' type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Search in lists and songs...">
                     <div class="input-group-append">
                         <button type="submit" class="btn btn-secondary btn-number">
-                            <i class="fas fa-search"></i>
+
                         </button>
+                        <img src="/images/search.svg" class="filter-primary inline mr-2" width="28" height="28" title="add" alt="">
                     </div>
                 </div>
             </form>
 
-            @else <h2 class="ml-2 header-dark">Create a new list</h2>
-            @endif
+
+            @endunless
         </div>
     </div>
 </header>
