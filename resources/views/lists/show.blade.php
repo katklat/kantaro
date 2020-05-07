61c2c119ef7d5f6e8662c03c0b3c3a77bad72157
katklat <kinz@pm.me>
2020-05-07 13:33:11 +0200

    Add link icons to lists.show.blade

--- resources/views/lists/show.blade.php
+++ resources/views/lists/show.blade.php
@@ -4,13 +4,29 @@
 <main>
     <div class="container text-center">
 
-
-        <div class="card my-4">
+        <div class="card my-3">
             <div class="card-body card-scroll text-left ">
-                <h4 class="card-title">Some List<h4>
-                        <h6 class="card-subtitle mb-2">May 2020, Hamburg</h6>
-                        <p class="card-text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque aliquam suscipit nobis odio error saepe, et ipsam labore, delectus asperiores quas sint quidem deserunt placeat consequatur quos, voluptate illum architecto?
-                        </p>
+                <div class="row">
+                    <div class="col-9">
+                        <h4 class="card-title">Some List<h4>
+                    </div>
+                    <div class="col-1 pl-1">
+                        <a href="{{ route('lists.tools') }}">
+                            <img src={{ asset('images/cloud.svg') }} class="filter-secondary" width="30" height="30" title="add" alt="">
+                        </a>
+                    </div>
+                    <div class="col-1 mr-1">
+                        <a href="{{ route('lists.edit') }}">
+                            <img src={{ asset('images/pencil.svg') }} class="filter-secondary" width="28" height="28" title="add" alt="">
+                        </a>
+                    </div>
+                </div>
+
+
+
+                <h6 class="card-subtitle mb-2">May 2020, Hamburg</h6>
+                <p class="card-text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque aliquam suscipit nobis odio error saepe, et ipsam labore, delectus asperiores quas sint quidem deserunt placeat consequatur quos, voluptate illum architecto?
+                </p>
             </div>
             <div class="card-footer p-0 justify-content-around">
                 <span class="card-icon">🔥</span>
